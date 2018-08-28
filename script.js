ba52ce0f9ef84c6dd34b3910c078005f5f6148f2
Md. Nafiul Hasan <nafiulhasanbd@gmail.com>
2018-08-28 14:59:05 +0600

    Activation JS

---
+++ script.js
@@ -0,0 +1,12 @@
+Here is the Script for Activation add this Script to theme.liquid file
+<script>
+  $('ul.tabs li').click(function(){
+     var tab_id = $(this).attr('data-tab');
+
+     $('ul.tabs li').removeClass('current');
+     $('.tab-content').removeClass('current');
+
+     $(this).addClass('current');
+     $("#"+tab_id).addClass('current');
+   })
+ </script>
